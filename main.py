e543d91172585001fd25921a4b540bc842a072c3
sophiasch2 <152403749+sophiasch2@users.noreply.github.com>
2024-02-07 18:22:48 +0000

    Notenbonus

--- main.py
+++ main.py
@@ -6,33 +6,49 @@ userAgent = {
 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
 }
 
-#while True:
-#print ("menu:")
-#print("1. Show available teams")
-#print("2.Select team and show high level information on the team")
-#print(" 3. Select team and and show all players of the team")
-#print(" 4. Stop the program")
-# userInput=
-#int (input("enter number: › "))
-#if userInput==1:
+while True:
+    print ("menu:")
+    print("1. Show available teams")
+    print("2. Select team and show information on the team")
+    print("3. Stop the program")
+    userInput=int(input("enter a number: > "))
 
-teamData = {}
+    if userInput==1:
+        url ="https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1"
+        r = requests.get(url, headers=userAgent)
+        if r.status_code == 200:
+            htmlText = r.text
+            htmlDocument = bs4.BeautifulSoup(htmlText, 'html.parser')
+            table =htmlDocument.find('table', {'class': 'items'}).find('tbody')
+            teams=table.find_all('tr')
 
-url ="https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1"
-r = requests.get(url, headers=userAgent)
-if r.status_code == 200:
-    htmlText = r.text
-    htmlDocument = bs4.BeautifulSoup(htmlText, 'html.parser')
+            for team in teams:
+                teamName=team.find('td',{'class': 'hauptlink no-border-links'}).get_text().strip()
 
-    table =htmlDocument.find('table', {'class': 'items'}).find('tbody')
-    teams=table.find_all('tr')
+                print(f"{teamName}")
 
-    for team in teams:
-#team=teams [0] (nur das erste)
+    if userInput ==2:
+        choice = int(input("index of the team. \n >>>"))
+        url ="https://www.transfermarkt.com/bundesliga/startseite/wettbewerb/L1"
+        r = requests.get(url, headers=userAgent)
+        if r.status_code == 200:
+            htmlText = r.text
+            htmlDocument = bs4.BeautifulSoup(htmlText, 'html.parser')
+            table =htmlDocument.find('table', {'class': 'items'}).find('tbody')
+            teams=table.find_all('tr')
+            
+        team=teams[choice]
         teamName=team.find('td',{'class': 'hauptlink no-border-links'}).get_text().strip()
         info = team.find_all('td', {'class': 'zentriert'})
         squad = info[1].text
         age = info[2].text
-        teamData[teamName] = {"squad": squad, "age": age}
-
+        foreigners = info[3].text
+        imfo = team.find_all('td', {'class': 'rechts'})
+        averageMarketValue = imfo[0].text
+        totalMarketValue = imfo[1].text
+        print(f"{teamName}: squad: {squad}, age: {age}, foreigners: {foreigners}, averageMarketValue: {averageMarketValue}, totalMarketValue: {totalMarketValue}")
+    
+    if userInput == 3:
+        break
+  
 
